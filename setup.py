86e004faee3dd75efd0b6df76ea249b3131f7226
WalzDS <david-simon.walz@basf.com>
2021-10-01 14:34:29 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -12,19 +12,36 @@ def get_version():
     return ""
 
 
+root_dir = os.path.dirname(__file__)
+with open(os.path.join(root_dir, "README.md"), "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name="basf-opti",
+    name="mopti",
     description="Tools for experimental design and multi-objective optimization",
-    url="https://gitlab.roqs.basf.net/bayesopt/opti",
+    author="BASF SE",
+    license="BSD-3",
+    url="https://github.com/basf/mopti",
+    keywords=[
+        "Bayesian optimization",
+        "Multi-objective optimization",
+        "Experimental design",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Scientific/Engineering",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     version=get_version(),
+    python_requires=">=3.7",
     packages=find_packages(),
     package_data={"": ["problems/data/*"]},
     include_package_data=True,
-    install_requires=[
-        "numpy",
-        "pandas",
-        "scipy>=1.7",
-    ],
+    install_requires=["numpy", "pandas", "scipy>=1.7"],
     extras_require={"testing": ["pytest", "scikit-learn"]},
-    python_requires=">=3.6",
 )
